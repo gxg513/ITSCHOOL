58b244045dd5978cda614f39c5c56a51731a6cbb
gxg513 <60469655+gxg513@users.noreply.github.com>
2022-03-05 16:55:29 +0200

    Create tomcat.sh

---
+++ tomcat.sh
@@ -0,0 +1,6 @@
+apt update -y
+apt install tomcat9 tomcat9-admin
+sudo sed -i 's/8080/9999/g' /var/lib/tomcat9/conf/server.xml
+sudo cat /var/lib/tomcat9/conf/server.xml | grep 9999
+
+systemctl restart tomcat9
